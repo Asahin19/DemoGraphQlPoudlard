e5dfcdcb1037b550857835acf4d9aeea70a8dda9
Nicolas <nicolas.sotty@cellenza.com>
2022-11-14 13:45:17 +0100

    add context

--- PoudlardAPI/DataModel/ApplicationDbContext.cs
+++ PoudlardAPI/DataModel/ApplicationDbContext.cs
@@ -6,17 +6,21 @@ namespace PoudlardAPI
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
-        {
-
-        }
+        {}
 
         public DbSet<Student> Students { get; set; }
+        public DbSet<House> Houses { get; set; }
+        public DbSet<Professor> Professors { get; set; }
+        public DbSet<Award> Awards { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<Student>(Student.BuildEntityType);
+            modelBuilder.Entity<House>(House.BuildEntityType);
+            modelBuilder.Entity<Professor>(Professor.BuildEntityType);
+            modelBuilder.Entity<Award>(Award.BuildEntityType);
         }
     }
 }
