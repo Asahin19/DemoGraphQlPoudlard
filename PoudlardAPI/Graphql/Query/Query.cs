1cc9ccd0ba8c8444df8ab6aad034475ebc5045f8
Nicolas <nicolas.sotty@cellenza.com>
2022-11-14 14:01:09 +0100

    query rename

--- PoudlardAPI/Graphql/Query/Query.cs
+++ PoudlardAPI/Graphql/Query/Query.cs
@@ -1,18 +1,21 @@
 ﻿using GraphQL;
 using GraphQL.Types;
+using Microsoft.EntityFrameworkCore;
 using PoudlardAPI.Graphql.Types;
 using System;
 using System.Linq;
 
 namespace PoudlardAPI.Graphql.Query
 {
-    public class StudentQuery : ObjectGraphType
+
+    public class Query : ObjectGraphType
     {
         private readonly ApplicationDbContext _appContext;
-        public StudentQuery(ApplicationDbContext appContext)
+        public Query(ApplicationDbContext appContext)
         {
             this._appContext = appContext;
             Name = "Query";
+
             Field<ListGraphType<StudentGraphType>>("students", "Return list of students", resolve: context => _appContext.Students.ToList());
 
             Field<StudentGraphType>("student", "Returns a Single Student",
